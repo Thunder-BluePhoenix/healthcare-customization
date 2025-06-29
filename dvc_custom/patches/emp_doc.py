17fed1af758a0d943cc43268fe7176cdb81799cf
RahulSarkar <bluephoenix00995@gmail.com>
2025-06-29 18:45:39 +0530

    feat: emp name patch txt file update

--- dvc_custom/patches/emp_doc.py
+++ dvc_custom/patches/emp_doc.py
@@ -1,11 +1,11 @@
 import frappe
 from frappe import _
 
+def execute():
+    emps = frappe.get_all("Employee", fields=["name", "employee_number"])
 
-emps = frappe.get_all("Employee", fields=["name", "employee_number"])
-
-for emp in emps:
-    if emp.employee_number != None or emp.employee_number != "":
-        new_name = f"{emp.employee_number}"
-        if emp.name != new_name:
-            frappe.rename_doc("Employee", emp.name, new_name, force=1, merge=False)
+    for emp in emps:
+        if emp.employee_number != None or emp.employee_number != "":
+            new_name = f"{emp.employee_number}"
+            if emp.name != new_name:
+                frappe.rename_doc("Employee", emp.name, new_name, force=1, merge=False)
